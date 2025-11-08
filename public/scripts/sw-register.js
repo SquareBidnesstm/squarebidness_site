78cc1c22deb13f16fa99b0a6566ca0e2b540fd26
Marcus Buckner <squarebidnessapparel@gmail.com>
2025-11-08 17:14:56 -0600

    Create sw-register.js

---
+++ public/scripts/sw-register.js
@@ -0,0 +1,9 @@
+// /public/scripts/sw-register.js
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("[SW] registered"))
+      .catch(err => console.warn("[SW] failed", err));
+  });
+}
