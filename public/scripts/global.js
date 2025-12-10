2f67964ef10146cc623f2fc26b0d513c979ca73c
Marcus Buckner <squarebidnessapparel@gmail.com>
2025-12-09 20:17:44 -0600

    Update global.js

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -3,7 +3,5 @@
 // Service Worker registration now lives in /scripts/sw-register.js.
 
 (() => {
-  // Keep this here so older script tags still load a valid file.
-  // Add any light site-wide JavaScript here in the future if needed.
   console.debug('[SB] global.js loaded');
 })();
